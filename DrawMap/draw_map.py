da8835339bfe8af1c95dd2b72e7128f098687c4a
Fuan <fuanho@gmail.com>
2021-09-17 18:52:03 +0800

    add raspberry pi pico send data example

--- DrawMap/draw_map.py
+++ DrawMap/draw_map.py
@@ -1,8 +1,6 @@
-import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import serial
-import numpy as np
 import math
 
 try:
@@ -10,29 +8,39 @@ try:
 except:
     print('no comport connection')
 
-figure = plt.figure()
+figure1 = plt.figure()
 
 x, y = [], []
+angle, distance = [], []
 
 
-def update(frame):
-    global figure
+def update1(frame):
+    global figure1, x, y, angle, distance
+    figure1.cla()
     x = []
     y = []
+    angle = []
+    distance = []
     for i in range(360):
         cc = str(ser.readline())
+        print(cc)
         if cc != '':
             cc = cc[2:][:-5]
             datas = cc.split(", ")
             try:
-                distance = float(datas[0])
-                angle = float(datas[1])
+                distance.append(float(datas[0]))
+                angle.append(float(datas[1]))
+                distance.append(0)
+                angle.append(0)
                 x.append(int(math.cos(math.radians(angle))*distance))
                 y.append(int(math.sin(math.radians(angle))*distance))
+                
             except:
                 print("input error")
-    figure = plt.scatter(x, y, alpha=0.5)
+    #figure1 = plt.scatter(x, y, alpha=0.5)
+    figure1 = plt.polar(angle, distance)
 
 
-animation = FuncAnimation(figure, update, interval=20)
+animation1 = FuncAnimation(figure1, update1, interval=1000)
+#animation2 = FuncAnimation(figure2, update2, interval=1000)
 plt.show()
